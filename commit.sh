a21b9e24d5073bfffb7700d2a51a0e2d48b4f4d6
Tiim <tim.bachmann96@gmail.com>
2020-03-17 12:32:48 +0100

    asdf

--- commit.sh
+++ commit.sh
@@ -6,7 +6,10 @@ set -e
 # navigate into the build output directory
 cd data
 
+
 git init
+git config user.email "github.action@example.com"
+git config user.name "GitHub Action"
 git add -A
 git commit --author="GitHub Action <action@github.com'>" -m 'deploy'
 
